9e4801c0f318c2fb785eaca01f5bc0264a4faa4e
hieu1999xl <hiu99xl@gmail.com>
2022-06-15 21:33:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+/* Place this script in <script> tag */
+
+function handle_MMqLrAhy2oN() {
+  document.getElementById("pano").src = "https://demos.evox.com/SquareSpaceWeb/X5/13751_in1024.html?wmode=opaque"; 
+  document.getElementById("pano").classList.add("fadeIn");
+ document.getElementById("iframeCover").classList.add("fadeOut");
+}
